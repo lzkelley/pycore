74dd4ba36277bc9715aa41da4fbc83e86dc06642
lzkelley <lkelley@cfa.harvard.edu>
2018-10-08 14:15:51 -0500

    Add full path to version file

--- pycore/__init__.py
+++ pycore/__init__.py
@@ -1,7 +1,14 @@
 """
 """
 
-with open('VERSION') as inn:
+import os
+
+PATH_VERSION = "VERSION"
+
+MODULE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '')
+
+path_version = os.path.join(MODULE_PATH, PATH_VERSION)
+with open(path_version, 'r') as inn:
     version = inn.read().strip()
 
 __version__ = version
